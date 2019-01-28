8314bb9e1dba1c6a796b8b49738ae3a649dcb1e9
Michal Biedzki <mbiedzki@me.com>
2019-01-28 16:42:50 +0100

    day1

---
+++ src/main/java/pl/coderslab/testMB.java
@@ -0,0 +1,21 @@
+package pl.coderslab;
+
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admins;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class testMB {
+
+    public static void main(String[] args) {
+
+        //test constructor
+        //Admins admin1 = new Admins("Michal", "Biedzki", "dsfhdhd", 1, true);
+        //System.out.println(admin1);
+
+
+
+    }
+}
