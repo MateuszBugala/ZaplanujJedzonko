e4698aafd86fc9ac265e95a00a5f804a6d349931
Michal Biedzki <mbiedzki@me.com>
2019-01-29 16:59:38 +0100

    dodanie stron Kontakt i O_Aplikacji

--- src/main/java/pl/coderslab/web/ServletContact.java
+++ src/main/java/pl/coderslab/web/ServletContact.java
@@ -1,8 +1,5 @@
 package pl.coderslab.web;
 
-import com.mysql.jdbc.StringUtils;
-import pl.coderslab.dao.RecipeDao;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,17 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/mbTestServlet")
-public class mbTestServlet extends HttpServlet {
+@WebServlet("/contact")
+public class ServletContact extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        int c = RecipeDao.countRecipeById(1);
-        String cStr = Integer.toString(c);
-        response.getWriter().append("recipe count ").append(cStr);
-
+        getServletContext().getRequestDispatcher("/contact.jsp").forward(request,response);
     }
 }
