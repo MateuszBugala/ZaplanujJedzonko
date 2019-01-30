4fce516f183805ead6a2ca3fc807a9cb5b8e4243
unlockpl <apiszczkiewicz@interia.pl>
2019-01-30 18:28:05 +0100

    lista plan i  stworzenie potrzebnych serwletow

--- src/main/java/pl/coderslab/web/RecipeDelFromRecipePlan.java
+++ src/main/java/pl/coderslab/web/RecipeDelFromRecipePlan.java
@@ -9,11 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/recipe/delete")
-public class RecipeDelete extends HttpServlet {
-
-
-
+@WebServlet("app/recipe/plan/delete")
+public class RecipeDeleteFromRecipePlan extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
