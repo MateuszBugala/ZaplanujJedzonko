dde4d8a5604fd3e51c92fff9273af663a48f4a12
Mateusz Bugala <mateusz.bugala@gmail.com>
2019-01-31 16:39:24 +0100

    added #16 Formularz edycji planu

--- src/main/java/pl/coderslab/web/PlanEdit.java
+++ src/main/java/pl/coderslab/web/PlanEdit.java
@@ -1,19 +1,14 @@
 package pl.coderslab.web;
 
 import pl.coderslab.dao.PlanDao;
-import pl.coderslab.model.Admins;
 import pl.coderslab.model.Plan;
-import pl.coderslab.utils.DbUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 
 @WebServlet("/app/plan/edit")
 public class PlanEdit extends HttpServlet {
@@ -36,6 +31,13 @@ public class PlanEdit extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
 
+        int planId = Integer.parseInt(request.getParameter("planId"));
+        Plan plan = PlanDao.readById(planId);
+        request.setAttribute("planFromDB",plan);
+
+        getServletContext().getRequestDispatcher("/app/plan/edit.jsp").forward(request, response);
     }
 }
