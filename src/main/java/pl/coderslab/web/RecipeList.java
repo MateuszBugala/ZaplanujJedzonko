db3050dded0c826a0d9ba39b3a2c994afe2d783d
unlockpl <apiszczkiewicz@interia.pl>
2019-01-31 18:49:02 +0100

    lista przepisow posortowana

--- src/main/java/pl/coderslab/web/RecipeList.java
+++ src/main/java/pl/coderslab/web/RecipeList.java
@@ -2,6 +2,7 @@ package pl.coderslab.web;
 
 import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.model.Recipe;
+import pl.coderslab.model.TimeComparator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @WebServlet("/app/recipe/list/")
@@ -21,8 +23,12 @@ public class RecipeList extends HttpServlet {
 
         List <Recipe> recipes = RecipeDao.findAll();
 
+        Collections.sort(recipes, new TimeComparator());
+
         request.setAttribute("recipes", recipes);
         getServletContext().getRequestDispatcher("/app/recipe/list.jsp").forward(request, response);
 
     }
+
+
 }
