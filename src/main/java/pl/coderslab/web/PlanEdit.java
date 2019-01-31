cf97049202c04fd6620877b715bf13a5fb6c701c
Mateusz Bugala <mateusz.bugala@gmail.com>
2019-01-31 15:37:57 +0100

    added #16 Obsługa formularza edycji planu

--- src/main/java/pl/coderslab/web/PlanEdit.java
+++ src/main/java/pl/coderslab/web/PlanEdit.java
@@ -1,17 +1,39 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.model.Admins;
+import pl.coderslab.model.Plan;
+import pl.coderslab.utils.DbUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 @WebServlet("/app/plan/edit")
 public class PlanEdit extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
 
+        String planName = request.getParameter("planName");
+        String planDescription = request.getParameter("planDescription");
+        int planId = Integer.parseInt(request.getParameter("planId"));
+
+        Plan editedPlan = PlanDao.readById(planId);
+        editedPlan.setName(planName);
+        editedPlan.setDescription(planDescription);
+
+        PlanDao.update(editedPlan);
+
+        response.sendRedirect("/app/plan/list");
     }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
